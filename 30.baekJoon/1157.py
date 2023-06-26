154c45a45fb14b3d983ac676d69a204805b23832
likemilktea <zerothese@gmail.com>
2023-06-26 20:46:49 +0900

    230626

--- 30.baekJoon/1157.py
+++ 30.baekJoon/1157.py
@@ -1,28 +1,8 @@
-str1 = input()
-# 입력받은 값을 대문자로 치환
-str1=str1.upper()
-# 각 값을 저장할 딕셔너리 생성
-dictCnt = {}
-# 딕셔너리에 값 추가
-for chr in str1:
-    dictCnt[chr] = str1.count(chr)
-# 최대값의 키와 밸류를 저장할 변수 선언
-maxValue=0
-maxKey=''
-# 최대값과 같은 개수를 확인할 변수 선언
-count = 0
-# 최대값 확인
-for key,value in dictCnt.items():
-    if value > maxValue:
-        maxValue = value
-        maxKey = key
-# 최대값의 개수 확인
-for value in dictCnt.values():
-    if value == maxValue:
-        count+=1
-# 최대값이 두 개 이상 존재한다면 출력값을 ?로
-if count>1:
-    maxKey='?'
-    
-print(maxKey)
-    
+print('''
+(___)
+(o o)____/
+ @@      \\
+  \\ ____,/
+  //   //
+ ^^   ^^
+''')
--- 30.baekJoon/1316.py
+++ 30.baekJoon/1316.py
@@ -1,22 +1,2 @@
-strList = []
-count = 0
-
-def checkGroupWord(str1):
-    strNew=str1[0] # strNew의 첫번째 값으로 str1의 첫번째 값을 입력
-    for chr in str1: # 반복문을 돌면서 str1의 값을 차례차례 strNew에 채워가면서
-        if (chr in strNew) and (chr != strNew[-1]): # 현재 문자가 strNew에 있으면서 마지막 문자인지 확인
-            return False # 조건에 해당한다면 False 리턴
-        strNew+=chr # 조건에 해당하지 않는다면 strNew에 chr을 입력
-    return True # 모든 반복이 끝날 때까지 False가 반환되지 않는다면 True를 반환
-
-# 몇 줄인지 입력 받음
-num1 = int(input())
-# num1만큼 반복하며 str입력받음
-for i in range(num1):
-    strList.append(input())
-# str갯수만큼 반복문을 돌면서 그룹워드인지 체크 후 카운트 증가
-for i in range(num1):
-    if checkGroupWord(strList[i]):
-        count+=1
-# 카운트 출력
-print(count)
+a,b =list(map(int,input().split()))
+print(':.32f'.format,a/b)
