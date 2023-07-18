6c1ce02cc768e19529626fb29d4a1c8feffe8142
likemilktea <86944264+likemilktea@users.noreply.github.com>
2023-07-18 12:47:05 +0900

    230718
    머신러닝
    앙상블 모델 실습 및 이론

--- 60.자습/인공지능사관학교.py
+++ 60.자습/인공지능사관학교.py
@@ -1,21 +1,25 @@
+def compression(n_list):
+    # 중복 제거 및 정렬
+    # [2 4 -10 4 -9]
+    # [-10 -9 2 4]
+    n_set=sorted(set(n_list))
+    
+    # 정렬된 값 딕셔너리로 순서값 입력
+    # [-10 -9 2 4]
+    # {-10:0 -9:1 2:2 4:3}
+    sorted_dict={cord : i for i, cord in enumerate(n_set)}
+        
+    # 딕셔너리로 map_nums 값 순서값 대입
+    # {-10:0 -9:1 2:2 4:3}
+    # [2 3 0 3 1]
+    map_nums=[sorted_dict[i] for i in n_list]
+    print(map_nums)
+
 n = int(input())
-m = list(map(int,input().split()))
-l= sorted(m)
-iLi=[]
-vLi=[]
-li=[]
-num=-1
-for i,v in enumerate(l):
-    if v not in vLi:
-        num+=1
+# [2 4 -10 4 -9]
+n_list = list(map(int, input().split()))
 
-    iLi.append(num)
-    vLi.append(v)
+compressed = compression(n_list)
 
-print(iLi)
-
-for i,v in zip(iLi,vLi):
-    for j in range(len(m)):
-        if m[j]==v:
-            m[j]=i
-print(m)
+for value in compressed:
+    print(value, end=' ')
