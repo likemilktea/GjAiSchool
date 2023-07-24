dae8a157153469028f1651f9fb5c05ff66b0cb94
likemilktea <86944264+likemilktea@users.noreply.github.com>
2023-07-24 17:30:09 +0900

    230724
    오픈CV
    영상파일 불러오기, 카메라 사용법 등

--- 60.자습/인공지능사관학교.py
+++ 60.자습/인공지능사관학교.py
@@ -1,29 +1,16 @@
-# 문제 : 뭐라고 하는지는 모르겠고 아무튼 작은 숫자에서 큰 숫자 순으로 순서를 매겨라.
+import sys
+sysinput = sys.stdin.readline
 
+N,M = map(int,sysinput().split())
 
+Nlist = [sysinput().rstrip() for i in range(N)]
+Ndict = {key+1 : value for key, value in enumerate(Nlist)}
+NdictSwap = {key : value+1 for value, key in enumerate(Nlist)}
 
-def compression(n_list):
-    # 중복 제거 및 정렬
-    # [2 4 -10 4 -9]
-    # [-10 -9 2 4]
-    n_set=sorted(set(n_list))
-    
-    # 정렬된 값 딕셔너리로 순서값 입력
-    # [-10 -9 2 4]
-    # {-10:0 -9:1 2:2 4:3}
-    sorted_dict={cord : i for i, cord in enumerate(n_set)}
-        
-    # 딕셔너리로 map_nums 값 순서값 대입
-    # {-10:0 -9:1 2:2 4:3}
-    # [2 3 0 3 1]
-    map_nums=[sorted_dict[i] for i in n_list]
-    print(map_nums)
-
-n = int(input())
-# [2 4 -10 4 -9]
-n_list = list(map(int, input().split()))
-
-compressed = compression(n_list)
-
-for value in compressed:
-    print(value, end=' ')
+Mlist = [sysinput().rstrip() for i in range(M)]
+print('=======================')
+for key in Mlist:
+    if key.isdigit():
+        print(Ndict[int(key)])
+    else:
+        print(NdictSwap[key])
